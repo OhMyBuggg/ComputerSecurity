0a4c981cb0921113b0fc06d337b56bbac9ea9612
jejewu <morywaysber@gmail.com>
2021-04-21 00:50:44 +0800

    translate slash by my own

--- project2/mitm_attack.py
+++ project2/mitm_attack.py
@@ -6,6 +6,7 @@ import time
 import threading
 import os
 from netaddr import IPAddress
+import math
 
 def get_mac(ip):
     for a in range(10):
@@ -34,6 +35,25 @@ def restore(destination_ip, source_ip):
     packet = scapy.ARP(op = 2, pdst = destination_ip, hwdst = destination_mac, psrc = source_ip, hwsrc = source_mac)
     scapy.send(packet, verbose = False)
 
+def translate_to_slash(netmask):
+    temp = netmask.rsplit('.')
+    number = 0
+    for i in range(4):
+        number = number << 8
+        number += int(temp[i])
+        # number = number << (8*i)
+        # print(number)
+    slash = 0
+    for i in range(32):
+        # print(number)
+        if number % 2 == 1:
+            slash = i
+            break
+        number = number >> 1
+    
+    return 32 - slash
+
+
 def get_local_info():
     interfaces = netifaces.ifaddresses('wlp3s0')
     # dictionary
@@ -41,8 +61,8 @@ def get_local_info():
     address = normal_internet['addr']
     netmask = normal_internet['netmask']
     broadcast = normal_internet['broadcast']
-    slash = IPAddress(netmask).netmask_bits()
-
+    # slash = IPAddress(netmask).netmask_bits()
+    slash = translate_to_slash(netmask)
     # obtain gateway
     gws = netifaces.gateways()
     gw = gws['default'][netifaces.AF_INET][0]
@@ -83,7 +103,7 @@ if __name__ == '__main__':
     # print(get_mac('192.168.99.100')) 
     getDevice(address + '/' + slash)
 
-    target_ip = "192.168.99.1" # Enter your target IP
+    target_ip = "192.168.99.241" # Enter your target IP
     # gateway_ip = "192.168.99.1" # Enter your gateway's IP
     gateway_ip = gw
     # t = threading.Thread(target = sslSplit)
--- project2/test.py
+++ project2/test.py
@@ -1,13 +1,30 @@
 import netifaces
 from netaddr import IPAddress
+import math
 
+# interfaces = netifaces.ifaddresses('wlp3s0')
+# # dictionary
+# normal_internet = interfaces[netifaces.AF_INET][0]
+# address = normal_internet['addr']
+# netmask = normal_internet['netmask']
+# broadcast = normal_internet['broadcast']
+# slash = IPAddress(netmask).netmask_bits()
 
-interfaces = netifaces.ifaddresses('wlp3s0')
-# dictionary
-normal_internet = interfaces[netifaces.AF_INET][0]
-address = normal_internet['addr']
-netmask = normal_internet['netmask']
-broadcast = normal_internet['broadcast']
-slash = IPAddress(netmask).netmask_bits()
-
-print(address + ' ' + netmask + ' ' + broadcast + ' ' + str(slash))
+# print(address + ' ' + netmask + ' ' + broadcast + ' ' + str(slash))
+netmask = "255.255.255.0"
+temp = netmask.rsplit('.')
+print(temp)
+number = 0
+for i in range(4):
+    number = number << 8
+    number += int(temp[i])
+    # number = number << (8*i)
+    print(number)
+slash = 0
+for i in range(32):
+    print(number)
+    if number % 2 == 1:
+        slash = i
+        break
+    number = number >> 1
+print( slash)
